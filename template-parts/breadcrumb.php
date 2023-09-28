7edea1fd185d8614546f07a8e8dcdd95d3160c78
y-miyatake <y.miyatake.2023@gmail.com>
2023-09-28 18:36:44 +0900

    wpテンプレートファイル修正（宮武）
    wpテンプレートファイル修正
    
    宮武

---
+++ template-parts/breadcrumb.php
@@ -0,0 +1,10 @@
+<div class="breadcrumb">
+    <div class="breadcrumb_inner">
+        <?php
+        if (function_exists("bcn_display")) {
+            //パンくずリストを出力
+            bcn_display();
+        }
+        ?>
+    </div>
+</div>
