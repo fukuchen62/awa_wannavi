2ca208bbd261af9502d0c799e15f45c3374d0f93
murakami-Web15 <qlip.gen15.murakami@gmail.com>
2023-10-12 15:28:01 +0900

    動物病院・レンタカーページ実装

---
+++ page-hospital.php
@@ -0,0 +1,229 @@
+<!-- header.phpを読み込む -->
+<?php get_header(); ?>
+
+<main>
+
+    <!-- いったんコンテナーに格納 -->
+    <div class="container ">
+
+
+
+        <!-- 帯になるコンテナーのタイトル -->
+        <div class="container">
+
+
+
+            <!-- 帯の部分 -->
+            <h2 class="h2__title mb20 fs35 bc_list">動物病院一覧</h2>
+
+            <!-- パンくずリスト -->
+
+            <div class="Breadcrumb"><a href="">>>動物病院一覧</a>
+            </div>
+
+
+            <!-- コンテンツのサブタイトル -->
+
+            <!-- 徳島市リスト-->
+            <h3 class="tbl__center">徳島市</h3>
+            <div class="tbl__border"></div>
+
+            <div class="tbl__center">
+
+                <div class="tbl__border"></div>
+                <div class="tbl__sp"></div>
+                <div class="tbl__main">
+                    <table class="tbl__li">
+                        <tr>
+                            <th>名称</th>
+                            <th>住所</th>
+                            <th>お問い合わせ
+                            </th>
+                        </tr>
+                        <tr class="line__solid">
+                        </tr>
+
+                        <tr>
+
+                            <td>徳島●●病院</td>
+                            <td>徳島市犬町わんこ番地12345</td>
+                            <td>000-000-0000</td>
+                        </tr>
+                        <tr class="line__dashed">
+                        </tr>
+                        <tr>
+                            <td>●●●●●●徳島●●病院</td>
+                            <td>徳島市犬町わんこ番地12345</td>
+                            <td>000-000-0000</td>
+                        </tr>
+                        <tr class="line__dashed">
+                        </tr>
+
+                        <tr>
+                            <td>●●●●●●●●●●●●●●病院</td>
+                            <td>徳島市犬町わんこ番地12345678910</td>
+                            <td>000-000-0000</td>
+                        </tr>
+                        <tr class="line__dashed">
+                        </tr>
+                    </table>
+                </div>
+                <div class="list__sp"></div>
+            </div>
+
+
+        </div>
+        <!-- 県東部リスト-->
+        <h3 class="tbl__center">県東部</h3>
+        <div class="tbl__border"></div>
+
+        <div class="tbl__center">
+
+
+            <div class="tbl__border"></div>
+            <div class="tbl__sp"></div>
+            <div class="tbl__main">
+                <table class="tbl__li">
+                    <tr>
+                        <th>名称</th>
+                        <th>住所</th>
+                        <th>お問い合わせ
+                        </th>
+                    </tr>
+                    <tr class="line__solid">
+                    </tr>
+
+                    <tr>
+
+                        <td>徳島●●病院</td>
+                        <td>徳島市犬町わんこ番地12345</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+                    <tr>
+                        <td>●●●●●●徳島●●病院</td>
+                        <td>徳島市犬町わんこ番地12345</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+
+                    <tr>
+                        <td>●●●●●●●●●●●●●●病院</td>
+                        <td>徳島市犬町わんこ番地12345678910</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+                </table>
+            </div>
+            <div class="list__sp"></div>
+        </div>
+
+        <!-- 県西部リスト-->
+        <h3 class="tbl__center">県西部</h3>
+        <div class="tbl__border"></div>
+
+        <div class="tbl__center">
+            <div class="tbl__border"></div>
+            <div class="tbl__sp"></div>
+            <div class="tbl__main">
+                <table class="tbl__li">
+                    <tr>
+                        <th>名称</th>
+                        <th>住所</th>
+                        <th>お問い合わせ
+                        </th>
+                    </tr>
+                    <tr class="line__solid">
+                    </tr>
+
+                    <tr>
+
+                        <td>徳島●●病院</td>
+                        <td>徳島市犬町わんこ番地12345</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+                    <tr>
+                        <td>●●●●●●徳島●●病院</td>
+                        <td>徳島市犬町わんこ番地12345</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+
+                    <tr>
+                        <td>●●●●●●●●●●●●●●病院</td>
+                        <td>徳島市犬町わんこ番地12345678910</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+                </table>
+            </div>
+            <div class="list__sp"></div>
+        </div>
+
+        <!-- 県南部リスト-->
+        <h3 class="tbl__center">県南部</h3>
+        <div class="tbl__border"></div>
+
+        <div class="tbl__center">
+
+            <div class="tbl__border"></div>
+            <div class="tbl__sp"></div>
+            <div class="tbl__main">
+                <table class="tbl__li">
+                    <tr>
+                        <th>名称</th>
+                        <th>住所</th>
+                        <th>お問い合わせ
+                        </th>
+                    </tr>
+                    <tr class="line__solid">
+                    </tr>
+
+                    <tr>
+
+                        <td>徳島●●病院</td>
+                        <td>徳島市犬町わんこ番地12345</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+                    <tr>
+                        <td>●●●●●●徳島●●病院</td>
+                        <td>徳島市犬町わんこ番地12345</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+
+                    <tr>
+                        <td>●●●●●●●●●●●●●●病院</td>
+                        <td>徳島市犬町わんこ番地12345678910</td>
+                        <td>000-000-0000</td>
+                    </tr>
+                    <tr class="line__dashed">
+                    </tr>
+                </table>
+            </div>
+            <div class="list__sp"></div>
+        </div>
+
+        <!-- もどるボタン -->
+
+        <div class="modoru__btn mb60">
+            <a href=""><img src="../assets/img/common/btn_modoru..svg"></a>
+        </div>
+
+
+    </div>
+
+</main>
+
+<!-- footer.phpを読み込む -->
+<?php get_footer(); ?>
