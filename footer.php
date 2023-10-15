e642cbfd46bf3e26b5d31917c475eba1b8d84a61
fukuchen62 <fukuchen62@gmail.com>
2023-10-15 21:21:23 +0900

    Update footer.php
    関連メニューの設定をしました。
    
    福島

--- footer.php
+++ footer.php
@@ -2,47 +2,47 @@
     <footer>
         <!-- js -->
         <script>
-            // 豆知識の配列を生成
+        // 豆知識の配列を生成
 
-            let mames = [];
+        let mames = [];
 
-            //console.log("確認表示");
+        //console.log("確認表示");
 
-            // 豆知識を配列$mamesから取り出す
-            mames[0] = "土佐犬みたいに徳島にも阿波犬っていたって知ってるワン？";
-            mames[1] = "祖谷のかずら橋は3年ごとに架け替えられてるワン";
-            mames[2] = "鳴門のうずしおは発生する時間が違うワン";
-            mames[3] = "日本一低い山は徳島県にある「弁天山」だワン！";
-            mames[4] = "実は電車は走っていなくて、全て汽車だワン！";
-            mames[5] = "電車が無いから、駅に自動改札が一台もないワン";
-            mames[6] = "スダチだけじゃなくて柚子もたくさん取れるワン";
-            mames[7] = "徳島県は、スポーツ射撃がすごく強いワン";
-            mames[8] = "「日本一入館料が高い」と言われる美術館があるワン";
-            mames[9] = "かずら橋は「祖谷のかずら橋」と「奥祖谷二重かずら橋」の２箇所あるワン";
-            mames[10] = "日本百名山にも名を連ねている「剣山」は西日本2位だワン";
-            mames[11] = "あわまるの阿波弁講座<br>　「せこい」は「お腹いっぱい」だワン";
-            mames[12] = "あわまるの阿波弁講座<br>　「ごっつい」は「すごい」だワン！ごっついワン";
-            mames[13] = "あわまるの阿波弁講座<br>　「おとろしい」は「恐ろしい」だワン";
-            mames[14] = "あわまるの阿波弁講座<br>　「もんてきぃ」は「帰っておいで」だワン";
-            mames[15] = "あわまるの阿波弁講座<br>　「いける？」は「大丈夫？」だワン!どこにも行かないワン";
-            mames[16] = "あわまるの阿波弁講座<br>　「かんまん」は「かまわない」だワン";
-            mames[17] = "あわまるの阿波弁講座<br>　「あるでないで」は「あるじゃないか」だワン";
-            mames[18] = "あわまるの阿波弁講座<br>　「えっとぶりで～」は「久しぶりだね～」だワン";
-            mames[19] = "あわまるの阿波弁講座<br>　「おもっしょい」は「おもしろい」だワン";
-            mames[20] = "あわまるの阿波弁講座<br>　「こんまいワンちゃん」は「小さいワンちゃん」だワン";
-            mames[21] = "あわまるの阿波弁講座<br>　「しわしわいきよ～」は「ゆっくり行きなよ～」だワン";
+        // 豆知識を配列$mamesから取り出す
+        mames[0] = "土佐犬みたいに徳島にも阿波犬っていたって知ってるワン？";
+        mames[1] = "祖谷のかずら橋は3年ごとに架け替えられてるワン";
+        mames[2] = "鳴門のうずしおは発生する時間が違うワン";
+        mames[3] = "日本一低い山は徳島県にある「弁天山」だワン！";
+        mames[4] = "実は電車は走っていなくて、全て汽車だワン！";
+        mames[5] = "電車が無いから、駅に自動改札が一台もないワン";
+        mames[6] = "スダチだけじゃなくて柚子もたくさん取れるワン";
+        mames[7] = "徳島県は、スポーツ射撃がすごく強いワン";
+        mames[8] = "「日本一入館料が高い」と言われる美術館があるワン";
+        mames[9] = "かずら橋は「祖谷のかずら橋」と「奥祖谷二重かずら橋」の２箇所あるワン";
+        mames[10] = "日本百名山にも名を連ねている「剣山」は西日本2位だワン";
+        mames[11] = "あわまるの阿波弁講座<br>　「せこい」は「お腹いっぱい」だワン";
+        mames[12] = "あわまるの阿波弁講座<br>　「ごっつい」は「すごい」だワン！ごっついワン";
+        mames[13] = "あわまるの阿波弁講座<br>　「おとろしい」は「恐ろしい」だワン";
+        mames[14] = "あわまるの阿波弁講座<br>　「もんてきぃ」は「帰っておいで」だワン";
+        mames[15] = "あわまるの阿波弁講座<br>　「いける？」は「大丈夫？」だワン!どこにも行かないワン";
+        mames[16] = "あわまるの阿波弁講座<br>　「かんまん」は「かまわない」だワン";
+        mames[17] = "あわまるの阿波弁講座<br>　「あるでないで」は「あるじゃないか」だワン";
+        mames[18] = "あわまるの阿波弁講座<br>　「えっとぶりで～」は「久しぶりだね～」だワン";
+        mames[19] = "あわまるの阿波弁講座<br>　「おもっしょい」は「おもしろい」だワン";
+        mames[20] = "あわまるの阿波弁講座<br>　「こんまいワンちゃん」は「小さいワンちゃん」だワン";
+        mames[21] = "あわまるの阿波弁講座<br>　「しわしわいきよ～」は「ゆっくり行きなよ～」だワン";
 
 
-            // ランダムに豆知識を表示させる
+        // ランダムに豆知識を表示させる
 
-            function msgOutput() {
-                // 変数mameに配列を代入
-                //変数ｒに、0以上1以下のランダムな数字とmameのlength(35）を掛けた結果（小数点を切り捨てた数字）を代入
-                let r = Math.floor(Math.random() * mames.length);
+        function msgOutput() {
+            // 変数mameに配列を代入
+            //変数ｒに、0以上1以下のランダムな数字とmameのlength(35）を掛けた結果（小数点を切り捨てた数字）を代入
+            let r = Math.floor(Math.random() * mames.length);
 
-                document.getElementById("awamaru_comment").innerHTML = mames[r]; //上の式で出た結果をHTMLで"awamaru_comment"に表示する
-                return false;
-            }
+            document.getElementById("awamaru_comment").innerHTML = mames[r]; //上の式で出た結果をHTMLで"awamaru_comment"に表示する
+            return false;
+        }
         </script>
         <div class="awamaru_trivia foot_scroll">
             <div class="awamaru_commentset fukidashi_animation">
@@ -85,8 +85,8 @@
                     /* <li><a href="<?php echo home_url('/rental_car/'); ?>">レンタカー（犬OK）</a></li> */
                     ?>
                     <li><a href="#">掲載施設一覧</a></li>
-                    <li><a href="<?php echo home_url('/about/'); ?>">このサイトについて</a></li>
-                    <li><a href="<?php echo home_url('/privacy-policy/'); ?>">プライバシーポリシー・<br>免責事項</a></li>
+                    <li><a href="<?php echo home_url('/spot_list/'); ?>">このサイトについて</a></li>
+                    <li><a href="<?php echo home_url('/privacy-policy/'); ?>">利用規約・免責事項</a></li>
                 </ul>
             </div>
             <div class="footer-largetext">
@@ -110,19 +110,19 @@
                     <li><a href="<?php echo home_url('/course/co01/'); ?>">海コース</a></li>
                     <li><a href="<?php echo home_url('/course/co03/'); ?>">山コース</a></li>
                 </ul>
-                <ul class="large-menu">特集記事
-                    <li><a href="#">マナー</a></li>
-                    <li><a href="#">犬の豆知識</a></li>
-                    <li><a href="#">お出かけ情報</a></li>
-                    <li><a href="#">特集</a></li>
+                <ul class="large-menu">おすすめ情報
+                    <li><a href="<?php echo home_url('/special_type/manner/'); ?>">マナー</a></li>
+                    <li><a href="<?php echo home_url('/special_type/dog-trivia/'); ?>">犬の豆知識</a></li>
+                    <li><a href="<?php echo home_url('/special_type/info/'); ?>">お出かけ情報</a></li>
+                    <li><a href="<?php echo home_url('/special_type/interview/'); ?>">特集</a></li>
                 </ul>
                 <ul class="large-menu">検索
-                    <li><a href="#">条件検索</a></li>
+                    <li><a href="<?php echo home_url('/filter/'); ?>">条件検索</a></li>
                 </ul>
-                <ul class="large-menu">新着記事
-                    <li><a href="#">イベント情報</a></li>
-                    <li><a href="#">お知らせ</a></li>
-                    <li><a href="#">更新情報</a></li>
+                <ul class="large-menu">新着情報
+                    <li><a href="<?php echo home_url('/category/news/'); ?>">お知らせ</a></li>
+                    <li><a href="<?php echo home_url('/category/event/'); ?>">イベント情報</a></li>
+                    <li><a href="<?php echo home_url('/category/update/'); ?>">更新情報</a></li>
                 </ul>
                 <ul class="large-menu">
                     <li><a href="#">マイページ</a></li>
