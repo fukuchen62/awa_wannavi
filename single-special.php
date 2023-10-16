5b7302eaabb85d7102317ab4b1414d57e612938c
Shu3UB <143670932+Shu3UB@users.noreply.github.com>
2023-10-16 15:40:19 +0900

    おすすめページをいくつか更新しました
    おすすめページをいくつか更新しました

--- single-special.php
+++ single-special.php
@@ -1,69 +1,55 @@
 <!-- header.phpを読み込む -->
 <?php get_header(); ?>
 
-<!-- パンくずリスト出力 -->
-<?php get_template_part("template-parts/breadcrumb"); ?>
+<main class="main">
+
+    <div class="container">
+
+        <!-- パンくずリスト -->
+        <?php get_template_part('template-parts/breadcrumb'); ?>
+
+        <!-- h2帯タイトル -->
+        <div class="container">
+
+            <!-- 帯になるコンテナーのタイトル -->
+            <div class="container pd20">
+                <h2 class="article__title">初心者マナー</h2>
+            </div>
+
+        </div>
+
+        <!-- 2col全体を格納するコンテナー -->
+        <div class="container__col">
+
+            <!-- メインコラムコンテナー -->
+            <div class="main__col">
+
+                <!-- コンテンツのサブタイトル -->
+                <h3 class="sub__ttl yellow"> <?php wp_title("") ?></h3>
+
+                <!-- カード型コンテンツ -->
+
+                <div class="card__2col">
 
 
-<!-- 帯になるコンテナーのタイトル -->
-<div class="container pd20">
-    <h2 class="article__title">初心者マナー</h2>
-</div>
 
 
-<!-- メインループの開始 -->
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : ?>
-        <?php the_post(); ?>
-
-        <main class="main">
-            <section class="sec">
-                <div class="container">
-                    <table>
-                        <tr>
-                            <th>タイトル</th>
-                            <td><?php the_field("title"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>概要</th>
-                            <td><?php the_field("outline"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>内容</th>
-                            <td><?php the_field("content"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>特集記事</th>
-                            <td><?php the_field("special_type"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>郵便番号</th>
-                            <td><?php the_field("post_code"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>住所</th>
-                            <td><?php the_field("address"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>電話番号</th>
-                            <td><?php the_field("tel"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>FAX</th>
-                            <td><?php the_field("fax"); ?></td>
-                        </tr>
-                        <tr>
-                            <th>Email</th>
-                            <td><?php the_field("email"); ?></td>
-                        </tr>
-                    </table>
                 </div>
-            </section>
-        </main>
 
-        <!-- メインループの終了 -->
-    <?php endwhile; ?>
-<?php endif; ?>
 
+            </div>
+
+            <!-- aside カテゴリー一覧 -->
+            <?php get_sidebar("special"); ?>
+
+        </div>
+    </div>
+
+
+
+
+
+
+</main>
 <!-- footer.phpを読み込む -->
 <?php get_footer(); ?>
--- taxonomy-special_type.php
+++ taxonomy-special_type.php
@@ -6,8 +6,8 @@
         <!-- h2帯タイトル -->
         <div class="container">
 
-            <div class="bnr__head bg__special”>
-            <h2 class=" page__title">おすすめ情報</h2>
+            <div class="banner__head">
+                <h2 class="page__title">おすすめ情報</h2>
             </div>
 
         </div>
@@ -21,9 +21,6 @@
             <!-- メインコラムコンテナー -->
             <div class="main__col">
 
-                <!-- 帯になるコンテナーのタイトル -->
-                <!-- <div class="container "> -->
-
                 <!-- コンテンツのサブタイトル -->
                 <h3 class="sub__ttl yellow"> 記事一覧</h3>
                 <!-- </div> -->
@@ -32,51 +29,7 @@
 
                 <div class="card__2col">
 
-                    <div class="card__wrapper mb40">
-                        <div class="card__block">
-                            <img class="card__img" src="../assets/img/cat.jpg" alt="">
-                            <div class="card__content">
-                                <div class="card__flex">
-                                    <div class="card__title">徳島●●●公園</div>
-                                    <div class="card__category">遊ぶ</div>
-                                </div>
 
-                                <div class="card__text">
-                                    <div class="card__address">●住所：徳島県徳島市鮎喰町２丁目５６－１１</div>
-                                    <div class="card__number">●お問い合わせ：０８０－６３７９－７４３７</div>
-                                    <div class="card__url"></div>
-                                </div>
-                            </div>
-                            <div class="card__link">
-                                <a href="#">もっと見る</a>
-                                <!-- <a href="#">FAQ</a>
-                                        <a href="#">PRIVACY</a> -->
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card__wrapper mb40">
-                        <div class="card__block">
-                            <img class="card__img" src="../assets/img/cat.jpg" alt="">
-                            <div class="card__content">
-                                <div class="card__flex">
-                                    <div class="card__title">徳島●●●公園</div>
-                                    <div class="card__category">遊ぶ</div>
-                                </div>
-
-                                <div class="card__text">
-                                    <div class="card__address">●住所：徳島県徳島市鮎喰町２丁目５６－１１</div>
-                                    <div class="card__number">●お問い合わせ：０８０－６３７９－７４３７</div>
-                                    <div class="card__url"></div>
-                                </div>
-                            </div>
-                            <div class="card__link">
-                                <a href="#">もっと見る</a>
-                                <!-- <a href="#">FAQ</a>
-                                        <a href="#">PRIVACY</a> -->
-                            </div>
-                        </div>
-                    </div>
 
                     <div class="card__wrapper mb40">
                         <div class="card__block">
